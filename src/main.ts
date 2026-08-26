9909c26a5d1e4b9a923ca516ab4d3f1781e6ecfa
natvy <natvyl4@gmail.com>
2026-08-26 16:43:55 -0700

    docs: documentar configuracion y aprendizaje de la actividad

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './styles/style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <main>
+  <h1>GIFinder</h1>
+  <p> Explora y ve el contenido multimedia</p>
+  </main>
+`;
